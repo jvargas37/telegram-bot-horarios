002c6411cbfcd1666cc979fff96586b9efa6fc18
kL4rK <jvargas37@hotmail.com>
2026-05-28 21:32:11 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,52 +9,36 @@ TOKEN = os.getenv("TOKEN")
 HORA_INICIO = 8
 HORA_FIN = 20
 
-estado_actual = None
-
 def es_cerrado():
     hora = datetime.now(pytz.timezone("Europe/Madrid")).hour
     return hora < HORA_INICIO or hora >= HORA_FIN
 
 async def controlar_grupo(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-    global estado_actual
     chat = update.effective_chat
 
-    cerrado = es_cerrado()
-
-    nuevo_estado = "cerrado" if cerrado else "abierto"
-
-    # 🔥 solo actúa si cambia el estado
-    if nuevo_estado == estado_actual:
-        return
-
-    estado_actual = nuevo_estado
-
     try:
-        if cerrado:
-            # 🔴 CERRAR GRUPO
+        if es_cerrado():
+            # 🔴 BLOQUEO TOTAL
             await chat.set_permissions(
                 ChatPermissions(
-                    can_send_messages=False
+                    can_send_messages=False,
+                    can_send_media_messages=False,
+                    can_send_polls=False,
+                    can_send_other_messages=False,
+                    can_add_web_page_previews=False
                 )
             )
-
-            await update.message.reply_text(
-                "⛔ GRUPO CERRADO\n"
-                "🕒 Horario: 08:00 - 20:00"
-            )
-
         else:
             # 🟢 ABRIR GRUPO
             await chat.set_permissions(
                 ChatPermissions(
-                    can_send_messages=True
+                    can_send_messages=True,
+                    can_send_media_messages=True,
+                    can_send_polls=True,
+                    can_send_other_messages=True,
+                    can_add_web_page_previews=True
                 )
-            )
-
-            await update.message.reply_text(
-                "🟢 GRUPO ABIERTO\n"
-                "🕒 Horario: 08:00 - 20:00"
             )
 
     except Exception as e:
