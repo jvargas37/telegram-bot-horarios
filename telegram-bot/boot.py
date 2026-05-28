e1164be8273eb9c40cd8d71c3ad032a51bba272d
kL4rK <jvargas37@hotmail.com>
2026-05-28 20:41:02 +0200

    boot.py

--- telegram-bot/boot.py
+++ telegram-bot/boot.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import Application, MessageHandler, filters, ContextTypes
 from datetime import datetime
 
-TOKEN = "PEGA_AQUI_TU_TOKEN"
+TOKEN = os.getenv("TOKEN")
 
 HORA_INICIO = 8
 HORA_FIN = 22
