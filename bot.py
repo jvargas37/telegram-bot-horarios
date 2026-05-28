f022c4df1484d2dc4901dc48794b759c8eb7ddea
kL4rK <jvargas37@hotmail.com>
2026-05-28 21:23:41 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,14 @@
+from telegram import Update
+from telegram.ext import Application, MessageHandler, filters, ContextTypes
+from datetime import datetime
+import pytz
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+HORA_INICIO = 8
+HORA_FIN = 20
+
 async def controlar_mensajes(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     hora = datetime.now(pytz.timezone("Europe/Madrid")).hour
@@ -12,3 +23,16 @@ async def controlar_mensajes(update: Update, context: ContextTypes.DEFAULT_TYPE)
             "⛔ Este grupo está cerrado fuera de horario.\n"
             f"🕒 Disponible de {HORA_INICIO}:00 a {HORA_FIN}:00"
         )
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(
+        MessageHandler(filters.TEXT & ~filters.COMMAND, controlar_mensajes)
+    )
+
+    print("Bot en marcha...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
